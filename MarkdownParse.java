3198dd6c4d06c833e623232800873c038823ed88
Andrina Zhang <78346223+Andrina-iris@users.noreply.github.com>
2022-07-07 10:40:11 -0700

    after lab4

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -28,10 +28,9 @@ public class MarkdownParse {
     public static void main(String[] args) throws IOException {
         Path fileName = Path.of(args[0]);
         String contents = Files.readString(fileName);
+		// System.out.println(contents); 
+		// file reading is correct
 	    ArrayList<String> links = getLinks(contents);
 		System.out.println(links);
-
     }
-
-
 }
--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,4 +1,12 @@
 import static org.junit.Assert.*;
+
+import java.beans.Transient;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.*;
 
 public class MarkdownParseTest {
@@ -6,4 +14,12 @@ public class MarkdownParseTest {
     public void addition() {
         assertEquals(2, 1 + 1);
     }
+
+    @Test
+    public void checkLinks() throws IOException {
+        MarkdownParse checkLink = new MarkdownParse();
+        System.out.println(List.of("https://something.com", "some-page.html").toString());
+        assertEquals(List.of("https://something.com", "some-page.html"), MarkdownParse.getLinks(Files.readString(Path.of("test-file.md"))));
+    }
+
 }
